6d9957dabe6e76848bf52895255d39fa93bf49c7
Quaker-Lee <tkacl2010@gmail.com>
2024-11-07 13:18:19 +0900

    🔥 Main파일 코드 삭제
    main파일 코드삭제 및 클래스 분리

---
+++ GenerateNumber/RandomNumber.swift
@@ -0,0 +1,19 @@
+//
+//  RandomNumber.swift
+//  NumberBaseballGame
+//
+//  Created by 이재건 on 11/6/24.
+//
+
+import Foundation
+
+class GenerateNumber {
+    func randomNumbers() -> [Int] {
+        var numbers = Set<Int>()
+        while numbers.count < 3 {
+            numbers.insert(Int.random(in: 1...9))
+        }
+        return Array(numbers)
+    }
+}
+
---
+++ NumberBaseballGame/BaseballGame.swift
@@ -0,0 +1,16 @@
+//
+//  BaseballGame.swift
+//  NumberBaseballGame
+//
+//  Created by 이재건 on 11/6/24.
+//
+
+import Foundation
+
+class NumberBaseballGame {
+    
+
+}
+
+
+
