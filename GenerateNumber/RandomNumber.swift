d78f9949f3a5ac38c232ce8fb8b4946a25d71ae5
Quaker-Lee <tkacl2010@gmail.com>
2024-11-08 21:04:27 +0900

    ✨ UserInput 분리 구현
    게임 이용자의 값을 받는 기능을 독립적인 class로 구현하였습니다.

--- GenerateNumber/RandomNumber.swift
+++ GenerateNumber/RandomNumber.swift
@@ -9,21 +9,11 @@ import Foundation
 
 // 이곳에서는 랜덤한 값을 생성합니다.
 class GenerateNumber {
-    func randomNumbers() {
+    func randomNumbers() -> [Int] {
         let numbers = (1...9).map { $0 } // 1부터 9까지 배열로 담음
         let shuffledNumbers = numbers.shuffled() // 셔플. 섞습니다.
 
-        print(shuffledNumbers[0...2]) // 섞은것의 첫번째부터 세번째 자리까지 출력
+        return [Int](shuffledNumbers[0...2]) //섞은 것을 Int배열로, 첫번째부터 세번째 값까지 리턴
     }
 }
 
-//구 코드
-//class GenerateNumber {
-//    func randomNumbers() -> [Int] {
-//        var numbers = Set<Int>()
-//        while numbers.count < 3 {
-//            numbers.insert(Int.random(in: 1...9))
-//        }
-//        return Array(numbers)
-//    }
-//}
--- NumberBaseballGame/BaseballGame.swift
+++ NumberBaseballGame/BaseballGame.swift
@@ -12,13 +12,12 @@ class BaseballGame {
         
     }
     
-//    func randomNumbers() -> [Int] {
-//        var numbers = Set<Int>()
-//        while numbers.count < 3 {
-//            numbers.insert(Int.random(in: 1...9))
-//        }
-//        return Array(numbers)
+//    func userInput() {
+//        let input = readLine() ?? "" //오류가 나서 nil이 들어오면 빈 문자열로 처리
+//        let stringArr = input.map { String($0) } //input을 문자열 배열로 변환.
+//        print(stringArr.compactMap { Int($0) }) //그것을 다시 Int 배열로 변환. compactMap으로 optional 벗기고, nil은 삭제함
 //    }
+
 }
 
 
